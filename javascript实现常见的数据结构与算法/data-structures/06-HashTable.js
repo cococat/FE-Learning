a1a43fb400aca01db9fcd21fd054d1199da4804f
Edgar.yjb <184515560@qq.com>
2016-07-05 15:34:13 +0800

    实现hash表，还未完成碰撞检测

--- javascript实现常见的数据结构与算法/data-structures/06-HashTable.js
+++ javascript实现常见的数据结构与算法/data-structures/06-HashTable.js
@@ -1,9 +1,15 @@
 /**
  * 散列表
  * 1. 存储数据的数组长度,通常取为质数,并且为了让散列更均匀,数组长度应该大于100? (why?)
+ *
  * 2. 散列函数的选择,依赖于键值得数据类型:
  *  > 若键值是整型,最简单常见的散列函数就是用数组长度对键值取余(除留余数法);
  *  > 键是字符串类型,散列函数选择比较难,需要慎重考虑
+ *
+ * 3. 碰撞检测。常用的方法:
+ *  > 开放寻址法
+ *  > 再散列法
+ *  > 链地址法(拉链法)
  * */
 
 function HashTable() {
@@ -37,31 +43,58 @@ HashTable.prototype = {
         return total % this._data.length;
     },
 
-    _showHashTableInfo: function () {
+    entries: function () {
+        var entryArr = [];
         for (var i = 0, l = this._data.length; i < l; i++) {
             if (this._data[i]) {
-                console.log(i + ": " + this._data[i]);
+                entryArr.push(this._data[i]);
             }
         }
+        return entryArr;
     },
 
-    put: function (ele) {
-        // var hashCode = this._simpleHash(ele);
-        var hashCode = this._betterHash(ele);
-        this._data[hashCode] = ele;
+    keys: function () {
+        var keysArr = [];
+        for (var i = 0, l = this._data.length; i < l; i++) {
+            if (this._data[i]) {
+                keysArr.push(this._data[i].key);
+            }
+        }
+        return keysArr;
     },
 
-    get: function (ele) {
+    values: function () {
+        var valuesArr = [];
+        for (var i = 0, l = this._data.length; i < l; i++) {
+            if (this._data[i]) {
+                valuesArr.push(this._data[i].value);
+            }
+        }
+        return valuesArr;
+    },
 
+    put: function (key, value) {
+        // var hashCode = this._simpleHash(key);
+        var hashCode = this._betterHash(key);
+        this._data[hashCode] = {key: key, value: value};
+    },
+
+    get: function (key) {
+        return this._data[this._betterHash(key)];
     }
-
-
 }
 
+
+/***
+ * 可能产生碰撞,碰撞检测算法暂时还未实现!
+ * @type {HashTable}
+ */
 var hashTable = new HashTable();
-hashTable.put('hello');
-hashTable.put('lleoh');
-hashTable.put('world');
-hashTable.put('hashTable');
-hashTable.put('hashMap');
-hashTable._showHashTableInfo();
+hashTable.put('hello', 'world');
+hashTable.put('hello2', 'world2');
+hashTable.put('foo', 'bar');
+hashTable.put('edgar', 'yijianbo');
+
+console.log(hashTable.keys());
+console.log(hashTable.values());
+console.log(hashTable.entries());
